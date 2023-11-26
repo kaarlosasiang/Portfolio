2c49288a932b2c99962fb80f7a7d6ab0edd50b1f
Kaarlo Sasiang <sasiang64@gmail.com>
2023-11-27 00:51:00 +0800

    updates

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
 feather.replace();
+
+function smoothScroll(target) {
+  document.querySelector(target).scrollIntoView({
+    behavior: "smooth",
+  });
+}
+
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent default anchor behavior
+    const targetId = event.target.getAttribute("href");
+    smoothScroll(targetId);
+  });
+});
