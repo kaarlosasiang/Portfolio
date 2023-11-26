64e52195238f4f58cc9c5ca9f0bcedd73a5f2b31
Kaarlo Sasiang <sasiang64@gmail.com>
2023-11-27 01:38:15 +0800

    updated header section

--- src/js/index.js
+++ src/js/index.js
@@ -13,3 +13,18 @@ document.querySelectorAll('a[href^="#"]').forEach((link) => {
     smoothScroll(targetId);
   });
 });
+
+let header = document.querySelector("header");
+console.log(header);
+window.addEventListener("scroll", function () {
+  // Check if the page has been scrolled
+  if (window.scrollY > 10) {
+    // Add the class when scrolled
+    header.classList.add("backdrop-blur-md");
+    header.classList.add("shadow-lg");
+  } else {
+    // Remove the class when at the top
+    header.classList.remove("backdrop-blur-md");
+    header.classList.remove("shadow-lg");
+  }
+});
